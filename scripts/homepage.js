390c5a90667d13360c52f6cf0eed4f3ce963a9ba
emceehyukie <emceehyukie@gmail.com>
2009-12-13 16:18:09 +0700

    new rotation engine for featured project added with fade transition effect

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,11 +1,16 @@
 //Script for Homepage
 $(function(){
-	$('#slide1').loopedSlider({
-		autoStart: 7000,
-		restart: 15000
-	});
 	$('#slide2').loopedSlider({
 		autoStart: 5000,
 		restart: 10000
 	});
+});
+$(function() {
+    $('#featuredcnt').cycle({
+        fx:      'fade,scrollLeft,scrollRight',
+        timeout: 10000, 
+    	speed:   1500,
+        prev:    '.previous',
+        next:    '.next, #slide1',
+    });
 });
